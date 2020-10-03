4f7c72e6fbb82287b58e1b99b928c39edc99e35a
Gergilim <gergilimbdo@gmail.com>
2020-10-03 03:35:04 -0300

    Added a serializer and view for the User model

--- AutoHomeDjango/settings.py
+++ AutoHomeDjango/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'SmartHomeAPI'
+    'SmartHomeAPI',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- SmartHomeAPI/views.py
+++ SmartHomeAPI/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# views.py
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import UserSerializer
+from .models import User
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('UserId')
+    serializer_class = UserSerializer
